7b6a5eb4c742a0d0dced0f78e2166baf8bcdfe54
ilia <ilia@glee.(none)>
2015-05-23 13:38:36 +0300

    Added switch for enums example

---
+++ 00_enum/src/main/java/EnumSwitch.java
@@ -0,0 +1,52 @@
+/**
+ * Created by ilia on 5/23/15.
+ */
+public class EnumSwitch {
+    public static void main(String[] args) {
+        EnumTest firstDay = new EnumTest(Day.MONDAY);
+        firstDay.tellItLikeItIs();
+        EnumTest thirdDay = new EnumTest(Day.WEDNESDAY);
+        thirdDay.tellItLikeItIs();
+        EnumTest fifthDay = new EnumTest(Day.FRIDAY);
+        fifthDay.tellItLikeItIs();
+        EnumTest sixthDay = new EnumTest(Day.SATURDAY);
+        sixthDay.tellItLikeItIs();
+        EnumTest seventhDay = new EnumTest(Day.SUNDAY);
+        seventhDay.tellItLikeItIs();
+    }
+
+    public enum Day {
+        SUNDAY, MONDAY, TUESDAY, WEDNESDAY,
+        THURSDAY, FRIDAY, SATURDAY
+    }
+
+    public static class EnumTest {
+        Day day;
+
+        public EnumTest(Day day) {
+            this.day = day;
+        }
+
+        public void tellItLikeItIs() {
+            switch (day) {
+                case MONDAY:
+                    System.out.println("Mondays are bad.");
+                    break;
+
+                case FRIDAY:
+                    System.out.println("Fridays are better.");
+                    break;
+
+                case SATURDAY:
+                case SUNDAY:
+                    System.out.println("Weekends are best.");
+                    break;
+
+                default:
+                    System.out.println("Midweek days are so-so.");
+                    break;
+            }
+        }
+
+    }
+}
